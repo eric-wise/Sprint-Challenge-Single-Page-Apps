86dd3fb6ed138485811df2c2f30a5f62e3587e25
Eric <eric@Erics-MacBook-Pro.local>
2019-08-18 22:39:27 -0500

    MVP

---
+++ src/components/AppRouter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom';
+import WelcomePage from './WelcomePage';
+import CharacterList from './CharacterList'
+import LocationsList from './LocationsList';
+import EpisodesList from './EpisodeList';
+
+export default function AppRouter() {
+  return <div className="page-view ui bottom attached segment active tab">
+    <Switch>
+      <Route path='/characters' component={CharacterList} />
+      <Route path='/locations' component={LocationsList} />
+      <Route path='/episodes' component={EpisodesList} />
+      <Route exact path='/' component={WelcomePage} />
+    </Switch>
+  </div>
+
+}
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  let [content, setContent] = useState(null);
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios.get('https://rickandmortyapi.com/api/character/')
+        .then(res => {
+          setContent(res.data);
+        });
+  }, []); // we don't need any dependencies for this
+
+  if(!content) return <div>Loading...</div>
 
   return (
-    <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+      <section className='character-list grid-view'>
+        {content.results.map(c => <CharacterCard key={c.name} character={c} />)}
+      </section>
   );
 }
